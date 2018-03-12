c7a3373eb4dc94ba6522b0d8a10f81fcefe2d12d
Emir Urcan <emir@Emir.local>
2018-03-12 13:10:22 +0300

    initial commit

---
+++ Assets/Scripts/EnemyBehaviourScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviourScript : MonoBehaviour {
+
+	public bool moveLeft;
+	public LayerMask groundLayer;
+	public Transform wallCheck;
+	public float wallCheckRadius;
+	public bool wallCollision;
+	public bool notAtEdge;
+	public Transform edgeCheck;
+	// Use this for initialization
+	void Start () {
+		
+		moveLeft = true;
+	
+	}
+
+	void FixedUpdate () {
+		wallCollision = Physics2D.OverlapCircle (wallCheck.position, wallCheckRadius, groundLayer);
+		notAtEdge = Physics2D.OverlapCircle (edgeCheck.position, wallCheckRadius, groundLayer);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var rB = GetComponent<Rigidbody2D> ();
+
+		if (wallCollision || !notAtEdge) {
+			moveLeft = !moveLeft;
+		}
+
+		if (moveLeft) {
+			transform.localScale = new Vector2 (1, 1);
+			rB.velocity = new Vector2 (-3, rB.velocity.y);
+		} 
+
+		else {
+			transform.localScale = new Vector2 (-1, 1);
+			rB.velocity = new Vector2 (3, rB.velocity.y);
+
+		}
+	}
+	void OnTriggerEnter2D (Collider2D other) {
+		if (other.name == "HeadStomper") {
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerBehaviourScript.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerBehaviourScript : MonoBehaviour {
+	private int lives=3;
+	public SpriteRenderer sprite;
+	public LayerMask groundLayer;
+	public Transform groundCheck;
+	public float groundCheckRadius;
+	private bool grounded;
+	private bool doubleJumped;
+	public CanvasGroup canvasYouWin;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void FixedUpdate () {
+		grounded = Physics2D.OverlapCircle (groundCheck.position, groundCheckRadius, groundLayer);
+
+	}
+	// Update is called once per frame
+	void Update () {
+		var rigidBody = GetComponent<Rigidbody2D> ();
+		var transform = GetComponent<Transform> ();
+
+		if (Input.GetKey ("right")) {
+			sprite.flipX = false; {
+				if (Input.GetKey ("left shift")) {
+					rigidBody.velocity = new Vector2 (10, rigidBody.velocity.y);
+				} else {
+					rigidBody.velocity = new Vector2 (5, rigidBody.velocity.y);
+				}
+			}
+		}
+
+		if (Input.GetKey ("left")) {
+			sprite.flipX = true; {
+				if (Input.GetKey ("left shift")) {
+					rigidBody.velocity = new Vector2 (-10, rigidBody.velocity.y);
+				} else {
+					rigidBody.velocity = new Vector2 (-5, rigidBody.velocity.y);
+				}
+			}
+
+		}
+
+		if (grounded)
+			doubleJumped = false;
+		if (Input.GetKeyDown ("up") && grounded) {
+					rigidBody.velocity = new Vector2 (rigidBody.velocity.x, 12);
+				
+			}
+
+		if (Input.GetKeyDown ("up") && !grounded && !doubleJumped) {
+			rigidBody.velocity = new Vector2 (rigidBody.velocity.x, 12);
+			doubleJumped = true;
+
+		}
+
+		if (transform.position.y < -6) {
+			lives -= 1;
+			if (transform.position.x < 37) {
+				transform.position = new Vector2 (-8, -3);
+
+			
+			} else {
+				transform.position = new Vector2 (36, 1);
+			}
+		}
+		if (lives == 0) {
+			SceneManager.LoadScene (SceneManager.GetActiveScene ().name);
+		}
+
+	}
+	public void OnTriggerEnter2D (Collider2D other) { 
+		if (other.name == "EnemyDamage") {
+			lives -= 1;
+			transform.position = new Vector2 (-8, -3);
+		} else if (other.name == "Exit") {
+			canvasYouWin.alpha = 1;
+		}
+
+	}
+}
